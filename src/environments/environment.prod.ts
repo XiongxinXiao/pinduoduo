15678db5951fa4219feee469476f9ed75a751fde
XiongxinXiao <xxiao079@uottawa.ca>
2020-02-26 23:48:52 -0500

    port and price change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:10010',
+  baseUrl: 'http://ec2-99-79-191-16.ca-central-1.compute.amazonaws.com:8080',
   //baseUrl: 'http://apis.imooc.com/api',
   icode: '6C67868AEB1EC926'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:10010',
+  baseUrl: 'http://ec2-99-79-191-16.ca-central-1.compute.amazonaws.com:8080',
   //baseUrl: 'http://apis.imooc.com/api',
   icode: '6C67868AEB1EC926'
 };
